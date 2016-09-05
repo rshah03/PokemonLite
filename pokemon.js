eca0b675721e7ac018f877a5748f7fea74f41a51
rshah03 <Ronak@MacBook-Air-2.local>
2016-09-05 18:04:03 -0500

    Initial commit | Front-end complete

---
+++ pokemon.js
@@ -0,0 +1,3 @@
+//Use table/map to show moveset?
+//Use linkedlist to show items in bag -- could use map/table to organize items by type??
+  //                                    |--> table/map of linkedlist
