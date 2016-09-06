36b566ee059b3c59b057379068ef573e8d956dfa
rshah03 <Ronak@MacBook-Air-2.local>
2016-09-05 22:40:19 -0500

    Init JavaScript file

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,16 @@
 //Use table/map to show moveset?
 //Use linkedlist to show items in bag -- could use map/table to organize items by type??
   //                                    |--> table/map of linkedlist
+yourMove = 0;
+
+
+function Pokemon(name, moveset, hp) {
+  this.name = name;
+  this.moveset = moveset;
+  this.hp = hp;
+}
+
+var Lugia = new Pokemon('Lugia', ['Aeroblast', 'Hidden Power', 'Weather Ball'], 100)
+function fight() {
+  console.log(Lugia);
+}
