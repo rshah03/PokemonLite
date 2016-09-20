6f987e93b090d41f8423f60852da9b02567238a3
rshah03 <Ronak@MacBook-Air-2.local>
2016-09-19 20:36:04 -0500

    Added critical hits

--- pokemon.js
+++ pokemon.js
@@ -23,9 +23,14 @@ var hiddenPower = new Move('Hidden Power', 13, 6);
 var dragonClaw = new Move('Dragon Claw', 12, 7);
 var hydropump = new Move('Hydropump', 15, 5);
 
-//var LugiaMoveset = [Move('Aero Blast', 20, 5), Move('Hidden Power', 13, 10), Move('Recover', -10, 10), Move('Hydropump', 15, 7)];
+
 var LugiaMoveset = [aeroBlast, hiddenPower, dragonClaw, hydropump];
-var Lugia = new Pokemon('Lugia', 'https://img.pokemondb.net/sprites/black-white/anim/back-normal/lugia.gif', 100, 'alive', true, LugiaMoveset);
+var Lugia = new Pokemon('Lugia',
+    'https://img.pokemondb.net/sprites/black-white/anim/back-normal/lugia.gif',
+    100,
+    'alive',
+    true,
+    LugiaMoveset);
 
 var pokemon = [Lugia]; //List of all pokemon -- implement this as a linked list
 var currentPokemon;
@@ -45,11 +50,23 @@ function isFainted(status) {
     }
 }
 
+function criticalHit() {
+    var chance = Math.floor(Math.random() * 11);
+    return chance;
+}
+
 function move1() {
 
     //TODO: Implement function above to calculate a critical chance
     var move = Lugia.moveset[0];
-    $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    var crit = criticalHit();
+    var critCompareTo = Math.floor(Math.random() * 3);
+    if (crit > critCompareTo) {
+        move.damage += 5;
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName + 'Critical hit!');
+    } else {
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    }
     // ^ Change this to be dynamic and fetch pokemonName from getCurrentPokemon
     hpAfterMove = move;
     if (move.pp !== 0) {
@@ -64,13 +81,21 @@ function move1() {
     }
     else
         $('#dialog-message').html(move.moveName + ' has no more PP left. Pick another...');
+    criticalHit();
 }
 
 function move2() {
 
     //TODO: Implement function above to calculate a critical chance
     var move = Lugia.moveset[1];
-    $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    var crit = criticalHit();
+    var critCompareTo = Math.floor(Math.random() * 3);
+    if (crit > critCompareTo) {
+        move.damage += 5;
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName + 'Critical hit!');
+    } else {
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    }
     // ^ Change this to be dynamic and fetch pokemonName from getCurrentPokemon
     hpAfterMove = move;
     if (move.pp !== 0) {
@@ -91,7 +116,14 @@ function move3() {
 
     //TODO: Implement function above to calculate a critical chance
     var move = Lugia.moveset[2];
-    $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    var crit = criticalHit();
+    var critCompareTo = Math.floor(Math.random() * 3);
+    if (crit > critCompareTo) {
+        move.damage += 5;
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName + 'Critical hit!');
+    } else {
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    }
     // ^ Change this to be dynamic and fetch pokemonName from getCurrentPokemon
     hpAfterMove = move;
     if (move.pp !== 0) {
@@ -112,7 +144,14 @@ function move4() {
 
     //TODO: Implement function above to calculate a critical chance
     var move = Lugia.moveset[3];
-    $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    var crit = criticalHit();
+    var critCompareTo = Math.floor(Math.random() * 3);
+    if (crit > critCompareTo) {
+        move.damage += 5;
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName + 'Critical hit!');
+    } else {
+        $('#dialog-message').html(Lugia.pokemonName + ' used ' + move.moveName);
+    }
     // ^ Change this to be dynamic and fetch pokemonName from getCurrentPokemon
     hpAfterMove = move;
     if (move.pp !== 0) {
